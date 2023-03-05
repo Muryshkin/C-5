95b5b76708cd1d963c808b4d0d584b882a41dbde
Aleksandr Muryshkin <muryshkin74@bk.ru>
2023-03-05 17:08:52 +0300

    ef

---
+++ A1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(100, 1000);
+
+
+    if (n >= 100 && n <= 999)
+    {
+        n = n % 2;
+        Console.Write($"{n} ");
+    }
+   
+    else 
+    {
+        Console.WriteLine("Не правельный ввод ");
+    }
+}
+return n;
+
+
+Console.WriteLine(string.Join(", ", arr));
---
+++ A2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+Random rnd = new Random();
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(1, 1001);
+
+if (n == arr[i]%2-1)
+{
+    n++;
+}
+else
+break;
+}
+return;
+void PrintArray(int[] n)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(n[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
